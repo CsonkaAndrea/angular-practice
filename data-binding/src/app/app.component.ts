410ed101afe644b50d8016212537a4eac9f19498
Csonka-Kis Andrea <cskand@gmail.com>
2019-08-01 09:31:00 +0200

    fist steps

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // megjelenik a htmel-ben elemként
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
 
+export class AppComponent {
+  title: string = `angular-practice-001`;
+  names: string[] = [
+    'pisti',
+    'ani',
+    'laci'
+  ];
+
+  name: string;
+  disabled: boolean = false;
+
+  getName(): string {
+    return `Bond, James Bond`;
+  }
+
+  constructor() {
+    this.name = this.name[0];
+    setInterval(() => {
+      const index: number = Math.floor(Math.random() * this.names.length);
+      // this.name = this.names[index];
+      //this.disabled = !this.disabled;
+    }, 2000);
+  }
+
+  setName(name): void {
+    this.name = name;
+  }
 }
+
